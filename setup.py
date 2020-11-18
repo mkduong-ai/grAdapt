a02edb1f9387f7017b7e606972439d95affbcd0d
Manh Khoi Duong (Jimmy) <maduo100@hhu.de>
2020-11-18 12:31:31 +0100

    0.1.1b6: deleted incompatible distributions

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='grAdapt',
-      version='0.1.1b5',
+      version='0.1.1b6',
       description='grAdapt: Gradient Adaption for Black-Box Optimization.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Manh Khoi Duong',
       author_email='manh.duong@hhu.de',
       url='https://github.com/mkduong-ai/grAdapt',
-      download_url = 'https://github.com/mkduong-ai/grAdapt/blob/master/dist/grAdapt-0.1.1b4.tar.gz?raw=true',
+      download_url = 'https://github.com/mkduong-ai/grAdapt/blob/master/dist/grAdapt-0.1.1b6.tar.gz?raw=true',
       keywords = ['grAdapt','black-box optimization', 'optimization', 'smbo', 'hyperparameter optimization', 'hyperparameter', 'sequential model-based optimization', 'stochastic optimization', 'global optimization', 'machine learning', 'toolbox'],
       license='Apache License 2.0',
       # py_modules=['grAdapt'],
